98c6f200fbd3458b78381800565590b8a923d1b2
Jon Davis <rocketshipx41@gmail.com>
2024-12-23 13:30:23 -0800

    Create class-festweb-activator.php

---
+++ includes/class-festweb-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://jonldavis.net
+ * @since      1.0.0
+ *
+ * @package    Festweb
+ * @subpackage Festweb/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Festweb
+ * @subpackage Festweb/includes
+ * @author     Jon Davis <rocketshipx41@gmail.com>
+ */
+class Festweb_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
