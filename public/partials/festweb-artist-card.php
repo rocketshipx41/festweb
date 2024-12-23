e49ea16661a67cdd287be27da0d50c4072ab2621
Jon Davis <rocketshipx41@gmail.com>
2024-12-23 13:35:37 -0800

    Create festweb-artist-card.php

---
+++ public/partials/festweb-artist-card.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-body">
+        <h4 class="card-title"><?= $artist->artist; ?></h4>
+        <p><?= smart_trim($artist->artist_description, 150); ?></p>
+        <ul><?= $artist->personnel; ?></ul>
+        <p><a href="<?= esc_url(add_query_arg(array('artist' => $artist->artist_slug),
+                home_url('/artist-profile/'))); ?>">See more</a></p>
+    </div>
+</div>
