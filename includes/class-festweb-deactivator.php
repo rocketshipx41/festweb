2d635dc98a73a65380bd3b6ca46277036afd9cdc
Jon Davis <rocketshipx41@gmail.com>
2024-12-23 13:31:48 -0800

    Create class-festweb-deactivator.php

---
+++ includes/class-festweb-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://jonldavis.net
+ * @since      1.0.0
+ *
+ * @package    Festweb
+ * @subpackage Festweb/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Festweb
+ * @subpackage Festweb/includes
+ * @author     Jon Davis <rocketshipx41@gmail.com>
+ */
+class Festweb_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
