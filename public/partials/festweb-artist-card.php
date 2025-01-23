0386258aee7a0355830eb2a44f735da25a7cba50
Jon Davis <rocketshipx41@gmail.com>
2025-01-22 16:29:56 -0800

    Update festweb-artist-card.php

--- public/partials/festweb-artist-card.php
+++ public/partials/festweb-artist-card.php
@@ -1,6 +1,7 @@
 <div class="card">
     <div class="card-body">
         <h4 class="card-title"><?= $artist->artist; ?></h4>
+        <img src="<?= $artist_img_path . $artist->photo; ?>" alt="<?= $artist->artist; ?>"/>
         <p><?= smart_trim($artist->artist_description, 150); ?></p>
         <ul><?= $artist->personnel; ?></ul>
         <p><a href="<?= esc_url(add_query_arg(array('artist' => $artist->artist_slug),
